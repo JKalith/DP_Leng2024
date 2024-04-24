6e198800fb76aca4e711435352ad55e400cc0995
JKalith <Jamesarias2383@gmail.com>
2024-04-24 15:50:43 -0600

    Subida de informacion a git

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+        //connectionString: "mongodb://localhost/next-js-registration-login-example",
+        connectionString: "mongodb+srv://jkalith:patitopelon2024@cluster0.9z8ba2e.mongodb.net/",
+        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+    },
+    publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000/api' // development api
+            : 'http://localhost:3000/api' // production api
+    }
+}
+
+module.exports = nextConfig
+
+
+
+
